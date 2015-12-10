dc5e1ffa6e33e4cc6b06e5aeef4da8c8eacbf879
Benoit Rapidel <benoit.rapidel@exmachina.fr>
2015-12-10 13:40:06 +0100

    Format emmc before dding

--- recipes-extended/emmc-flasher/files/emmc-flasher.sh
+++ recipes-extended/emmc-flasher/files/emmc-flasher.sh
@@ -25,10 +25,32 @@ if [[ "x$confirm" != "xy" ]]; then
     exit 2
 fi
 
+IMG_PARTS=$(fdisk -l -o Start,Size,Id $IMG | tail -n2)
+BOOT_SECT=$(echo $IMG_PARTS | cut -d" " -d1-3 | sed 's/ /,/g')
+
+sfdisk $TARGET_MMC << EOF
+$BOOT_SECT,*
+,,,-
+EOF
+
+sync
+
+BOOT_PART="${TARGET_EMMC}p0"
+ROOT_PART="${TARGET_EMMC}p1"
+
+mkfs.fat "${BOOT_PART}"
+mkfs.ext4 "${ROOT_PART}"
+
+sync
+
 echo "Writing $IMG to $TARGET_MMC…"
-dd if="./$IMG" of="$TARGET_MMC" bs=16M && \
+dd if="./$IMG" skip=$(echo $BOOT_SECT | cut -d, -f1) of=$BOOT_PART bs=512 count=$(echo $BOOT_SECT | cut -d, -f2)
+dd if="./$IMG" skip=$(echo $ROOT_SECT | cut -d, -f1) of=$ROOT_PART bs=512
 echo "Writing $IMG to $TARGET_MMC… Done." || exit 3
 
-echo "Press any key to reboot. Please remove sdcard."
+sync
+
+echo
+echo "Press [Enter] to reboot. Please remove sdcard after poweroff."
 read reboot
 reboot
